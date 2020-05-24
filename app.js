1905e8bfc4e35342246b547a7adfbe3afd9f5302
Lukas Siemoneit <hello@lsiemoneit.de>
2020-05-24 13:55:56 +0200

    added click functionality and custom cursor

--- app.js
+++ app.js
@@ -7,9 +7,13 @@
 // console.log(angleRadians);
 // console.log(angleDegrees);
 
-gsap.from(".circle-1", {duration: 2, x: 1000, y: -1000, ease: "slow"});
-gsap.from(".circle-2", {duration: 2, x: -1000, y: -1000, ease: "slow"});
-gsap.from(".circle-3", {duration: 2, x: -1000, y: 1000, ease: "slow"});
+gsap.set(".circle-1", {x: "-1500", y: "-1000", ease: "slow"});
+gsap.set(".circle-2", {x: "-2000", y: "1000", ease: "slow"});
+gsap.set(".circle-3", {x: "3000", y: "2000", ease: "slow"});
+
+gsap.to(".circle-1", {duration: 2, x: "5vw", y: "-30vh", ease: "slow"});
+gsap.to(".circle-2", {duration: 2, x: "-40vw", y: "20vh", ease: "slow"});
+gsap.to(".circle-3", {duration: 2, x: "50vw", y: "50vh", ease: "slow"});
 
 const min = 0;
 const max = 255;
@@ -32,3 +36,86 @@ circle3 = document.querySelector('.circle-3');
 circle1.style.backgroundColor = `rgb(${r1}, ${b1}, ${g1})`;
 circle2.style.backgroundColor = `rgb(${r2}, ${b2}, ${g2})`;
 circle3.style.backgroundColor = `rgb(${r3}, ${b3}, ${g3})`;
+
+let i = 0;
+
+document.addEventListener('click', (event) => {
+    let width = circle1.offsetWidth;
+    console.log(event.clientX);
+    console.log(event.clientY);
+    console.log("width:" + width);
+    
+    
+    if(i === 0 ) {
+        let r1 = Math.floor(Math.random() * (max - min)) + min;
+        let g1 = Math.floor(Math.random() * (max - min)) + min;
+        let b1 = Math.floor(Math.random() * (max - min)) + min;
+        gsap.to(circle1, {duration: 1, x: event.clientX - width / 2, y: event.clientY - width / 2});
+        circle1.style.backgroundColor = `rgb(${r1}, ${b1}, ${g1})`;
+    }
+
+    if(i === 1 ) {
+        let r1 = Math.floor(Math.random() * (max - min)) + min;
+        let g1 = Math.floor(Math.random() * (max - min)) + min;
+        let b1 = Math.floor(Math.random() * (max - min)) + min;
+        gsap.to(circle2, {duration: 1, x: event.clientX - width / 2, y: event.clientY - width / 2});
+        circle2.style.backgroundColor = `rgb(${r1}, ${b1}, ${g1})`;
+    }
+
+    if(i === 2 ) {
+        let r1 = Math.floor(Math.random() * (max - min)) + min;
+        let g1 = Math.floor(Math.random() * (max - min)) + min;
+        let b1 = Math.floor(Math.random() * (max - min)) + min;
+        gsap.to(circle3, {duration: 1, x: event.clientX - width / 2, y: event.clientY - width / 2});
+        circle3.style.backgroundColor = `rgb(${r1}, ${b1}, ${g1})`;
+    }
+
+    i++;
+
+    if(i>2){
+        i = 0;
+    }
+
+});
+
+const cursor = document.querySelector('.cursor');
+
+console.log(cursor);
+
+window.addEventListener('mousemove', e => {
+    cursor.style.top = e.pageY + "px";
+    cursor.style.left = e.pageX + "px";
+});
+
+window.addEventListener('scroll', e => {
+    console.log("firing!");
+});
+
+let scale = 1;
+let scaleCounter = 0;
+
+window.addEventListener('wheel', e => {
+    const delta = Math.sign(event.deltaY);
+    // console.info(e);
+
+    
+
+    if(delta === 1) {
+        scaleCounter = scaleCounter*1.1 + 1;
+    } else {
+        scaleCounter = scaleCounter*0.9 - 1;
+    }
+
+    if(scaleCounter < -8){
+        scaleCounter = -8;
+    }
+    if(scaleCounter > 250) {
+        scaleCounter = 250;
+    }
+
+    scale = 1 + scaleCounter/10;
+
+    cursor.style.transform = `scale(${scale})`
+
+    console.info(scaleCounter);
+});
